3c846e89cdb943a1d428971acad1502ba038dc00
mehatakat <mehtakat@sheridanc.on.ca>
2020-02-19 16:22:59 -0500

    Firs Commit

---
+++ src/pushtogit/PushToGit.java
@@ -0,0 +1,27 @@
+ /*
+        Name:  [your name here]
+        Assignment:  [assignment name]
+        Program: [your program name here]
+        Date:  [assignment due date here]
+    
+        Description:
+        [program description in your own words]
+    */
+package pushtogit;
+
+/**
+ *
+ * @author Katrina
+ */
+public class PushToGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("My First commit");
+    }
+
+}
